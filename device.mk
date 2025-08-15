b2837cc20232a0155396f0f15f4a4cd83a66c011
WenHao2130 <wenhao2130@outlook.com>
2025-08-15 16:52:17 +0800

    555
    Change-Id: I01ff514bf487e2c65182112247806c3f136a664b
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- device.mk
+++ device.mk
@@ -314,7 +314,8 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
     android.hardware.sensors@1.0-service \
-    android.frameworks.sensorservice@1.0
+    android.frameworks.sensorservice@1.0 \
+    android.frameworks.sensorservice@1.0.vendor
 
 # Tetheroffload
 PRODUCT_PACKAGES += \
