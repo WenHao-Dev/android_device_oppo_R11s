2f900df512e7c9c656a4cc5c03fc30f9ab963305
WenHao2130 <wenhao2130@outlook.com>
2025-08-15 13:46:55 +0800

    555
    Change-Id: If7b2dacdca4adb1535c86b162581c70b16222dc3
    Signed-off-by: WenHao2130 <wenhao2130@outlook.com>

--- device.mk
+++ device.mk
@@ -61,8 +61,7 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    android.hardware.audio@2.0.service \
-    android.hardware.audio@2.0-impl \
+    android.hardware.audio.service \
     android.hardware.audio@5.0.vendor \
     android.hardware.audio@5.0-impl \
     android.hardware.audio.effect@5.0-impl \
